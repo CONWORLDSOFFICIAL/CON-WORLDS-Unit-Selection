955e23546ba575145d05d5fa00e6319efee346c2
CONWORLDSOFFICIAL <mk.virus.agar.io@gmail.com>
2025-02-13 22:01:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyArxYCguc3tHkx_R25E1l8aPffnDZjgodE",
   authDomain: "con-worlds-unit-selection.firebaseapp.com",
   projectId: "con-worlds-unit-selection",
-  storageBucket: "con-worlds-unit-selection.firebasestorage.app",
+  storageBucket: "con-worlds-unit-selection.appspot.com",
   messagingSenderId: "1056963708496",
   appId: "1:1056963708496:web:09c7e564bc2d06ed4ef355",
   measurementId: "G-P7TXFN0PDQ"
