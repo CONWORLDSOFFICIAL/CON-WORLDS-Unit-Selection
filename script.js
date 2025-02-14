9d37af0c58a64a67b4c651f602ce6950951e4423
CONWORLDSOFFICIAL <mk.virus.agar.io@gmail.com>
2025-02-13 21:50:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyArxYCguc3tHkx_R25E1l8aPffnDZjgodE",
+  authDomain: "con-worlds-unit-selection.firebaseapp.com",
+  projectId: "con-worlds-unit-selection",
+  storageBucket: "con-worlds-unit-selection.firebasestorage.app",
+  messagingSenderId: "1056963708496",
+  appId: "1:1056963708496:web:09c7e564bc2d06ed4ef355",
+  measurementId: "G-P7TXFN0PDQ"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+
 document.getElementById("unitForm").addEventListener("submit", function(event) {
     event.preventDefault(); // Stop form submission
 
